ba90b47ec64731925a867cf0462a9f0ceb06443c
Kenechukwu <geraldinyiama21@gmail.com>
2022-11-18 10:06:18 +0100

    Added tests

--- test/server_test.dart
+++ test/server_test.dart
@@ -1,12 +1,22 @@
 import 'dart:io';
 
 import 'package:http/http.dart';
+import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
+
+import '../bin/models/news_model.dart';
+import '../bin/services/news_api_service/gnews_api_factory.dart';
+import '../bin/services/news_api_service/news_api_interface.dart';
+
+class MockNewsService extends Mock implements NewsApiInterface {}
 
 void main() {
   final port = '8080';
   final host = 'http://0.0.0.0:$port';
   late Process p;
+
+  late GNewsApiFactory sut;
+  late MockNewsService mockService;
 
   setUp(() async {
     p = await Process.start(
@@ -20,14 +30,43 @@ void main() {
 
   tearDown(() => p.kill());
 
-  test('Root', () async {
-    final response = await get(Uri.parse('$host/'));
-    expect(response.statusCode, 200);
-    expect(response.body, 'Hello, World!\n');
+  group("getArticles", () {
+    final mockedNewsFeed = NewsFeed(feed: [
+      NewsModel(title: 'Title 1', content: 'Content 1'),
+      NewsModel(title: 'Title 2', content: 'Content 2'),
+      NewsModel(title: 'Title 3', content: 'Content 3'),
+    ]);
+
+    sut = GNewsApiFactory();
+
+    void getNewsServiceReturnsArticles() {
+      when(() => sut.getNewsArticles()).thenAnswer(
+        (realInvocation) => Future<NewsFeed>.value(mockedNewsFeed),
+      );
+    }
+
+    test("get articles from news api", () async {
+      getNewsServiceReturnsArticles();
+
+      await sut.getNewsArticles();
+      verify(() => sut.getNewsArticles()).called(1);
+    });
+
+    test("confirm loading state of test call", () async {
+      getNewsServiceReturnsArticles();
+
+      
+    });
   });
 
-  test('404', () async {
-    final response = await get(Uri.parse('$host/foobar'));
-    expect(response.statusCode, 404);
-  });
+  // test('Root', () async {
+  //   final response = await get(Uri.parse('$host/'));
+  //   expect(response.statusCode, 200);
+  //   expect(response.body, 'Hello, World!\n');
+  // });
+
+  // test('404', () async {
+  //   final response = await get(Uri.parse('$host/foobar'));
+  //   expect(response.statusCode, 404);
+  // });
 }
